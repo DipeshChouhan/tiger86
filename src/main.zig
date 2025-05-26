435f68deecd53c87836624f261b030898a88e7f7
DipeshChouhan <akku00009@gmail.com>
2025-05-26 11:19:05 +0530

    changed README.md and src/main.zig to show correct version of ibm pc

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,8 @@
-// A IBM PC emulator
+// A IBM PS/2 Model 30 emulator
 
 const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Tiger86 is an IBM PC emulator.\n", .{});
+    try stdout.print("Tiger86 is an IBM PS/2 Model 30 emulator.\n", .{});
 }

f803ea47d9f10d602127f70b205a75a49aff4ef0
DipeshChouhan <akku00009@gmail.com>
2025-05-28 20:39:08 +0530

    added tests to test flagsClear and flagsSet functions in utils.zig file

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,14 @@
 // A IBM PS/2 Model 30 emulator
 
 const std = @import("std");
+const utils = @import("processor/utils.zig");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Tiger86 is an IBM PS/2 Model 30 emulator.\n", .{});
+
+    var flags: u16 = 1;
+
+    utils.flagsClearAll(&flags);
+    try stdout.print("Flags: {d}\n", .{flags});
 }
--- src/processor/utils.zig
+++ src/processor/utils.zig
@@ -1,3 +1,4 @@
+// TODO: Test the functions for correctness
 const mask: u16 = 1;
 const FlagsType = enum {
     CF,
@@ -41,4 +42,42 @@ pub inline fn flagsSet(comptime flag: FlagsType, flags: *u16) void {
         .DF => flags.* |= mask << 10,
         .OF => flags.* |= mask << 11,
     }
+}
+
+const std = @import("std");
+
+// write a test for flagsClear function
+test "flagsClear" {
+    var flags: u16 = 0b1111_1111_1111_1111;
+    flagsClear(.CF, &flags);
+    try std.testing.expectEqual(flags, 0b1111_1111_1111_1110);
+
+    flagsClear(.PF, &flags);
+    try std.testing.expectEqual(flags, 0b1111_1111_1111_1010);
+
+    flagsClear(.AF, &flags);
+    try std.testing.expectEqual(flags, 0b1111_1111_1110_1010);
+
+    flagsClear(.OF, &flags);
+    try std.testing.expectEqual(flags, 0b1111_0111_1110_1010);
+}
+
+// write a test for flagsSet function
+
+test "flagsSet" {
+    var flags: u16 = 0b0000_0000_0000_0000;
+    flagsSet(.CF, &flags);
+    try std.testing.expectEqual(flags, 0b0000_0000_0000_0001);
+
+    flagsSet(.PF, &flags);
+    try std.testing.expectEqual(flags, 0b0000_0000_0000_0101);
+
+    flagsSet(.AF, &flags);
+    try std.testing.expectEqual(flags, 0b0000_0000_0001_0101);
+
+    flagsSet(.OF, &flags);
+    try std.testing.expectEqual(flags, 0b0000_1000_0001_0101);
+
+    flagsSet(.ZF, &flags);
+    try std.testing.expectEqual(flags, 0b0000_1000_0101_0101);
 }

5afb52ff59ba704256875e66f4dd7621b82b860c
DipeshChouhan <akku00009@gmail.com>
2025-05-31 23:35:00 +0530

    decoder structure is done

--- src/processor/decoder.zig
+++ src/processor/decoder.zig
@@ -1,22 +1,18 @@
 // The InstructionName_hex is used so that it can be reference in 8086 family user manual Table 4-13 Machine Instruction Decoding Guide
 const Biu = @import("biu.zig").Biu;
-const Instruction = @import("instruction.zig").Instruction;
-const InstType = enum(u8) {
-    ADD_00 = 0x00,
-    ADD_01 = 0x01,
-    ADD_02 = 0x02,
-    ADD_03 = 0x03,
-    ADD_04 = 0x04,
-    ADD_05 = 0x05,
-
-    PUSH_06 = 0x06,
-    POP_07 = 0x07,
-};
+const insts = @import("instruction.zig");
+const Instruction = insts.Instruction;
 
 const Decoder = struct {
     biu: *Biu, // a pointer to bus interface unit
 
     // decode the instruction
     // param inst: a pointer to empty Instruction struct which get's filled by the decoder
-    fn decode(self: *Decoder, inst: *Instruction) void {}
+    fn decode(self: *Decoder, inst: *Instruction) void {
+        const opcode = self.*.biu.fetch();
+
+        switch (opcode) {
+            .ADD_00 => {},
+        }
+    }
 };
--- src/processor/instructions.zig
+++ src/processor/instructions.zig
@@ -1,4 +1,15 @@
-const InstructionName = enum {};
+const InstructionType = enum(u8) {
+    ADD_00 = 0x00,
+    ADD_01 = 0x01,
+    ADD_02 = 0x02,
+    ADD_03 = 0x03,
+    ADD_04 = 0x04,
+    ADD_05 = 0x05,
+
+    PUSH_06 = 0x06,
+    POP_07 = 0x07,
+};
+
 pub const Instruction = struct {
-    name: InstructionName,
+    instType: InstructionType,
 };
